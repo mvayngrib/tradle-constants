20bc5002394537aab96a36482dbd1e628abf3397
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-29 12:17:43 -0400

    more constants

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
+var DATA_ARG_NAME = 'data'
+
 module.exports = {
+  IDENTITY_PUBLISH_ADDRESS: 'mhKnKtPFCbYpC61buDMgSBB57mqiWvXCUo',
+  DATA_ARG_NAME: DATA_ARG_NAME,
+  JSON_CONTENT_DISP: 'Content-Disposition: form-data; name="' + DATA_ARG_NAME + '"',
+  DEFAULT_BOUNDARY: '--------------------------000000000000000000000000',
+  curHash: '_c',
+  prevHash: '_p',
   rootHash: '_r',
-  currentHash: '_c',
+  owner: '_o',
+  nextOwner: '_n',
   sig: '_s'
 }
