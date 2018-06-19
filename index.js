ee9dad7e896895513c2b26e53299b6435986ed1a
Mark Vayngrib <mark@tradle.io>
2018-06-19 10:58:56 -0400

    add ORG, ORG_SIG

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ module.exports = {
   PREV_TO_RECIPIENT: '_q',
   AUTHOR: '_author',
   RECIPIENT: '_recipient',
+  ORG: '_org',
+  ORG_SIG: '_orgsig',
   TIMESTAMP: '_time',
   VERSION: '_v',
   PREVHEADER: '_ph',
